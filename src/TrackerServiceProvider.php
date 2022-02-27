c569910a192ec21972fc7e522e57546a695ac034
Joey J <joey.jaulimsing@live.com>
2022-02-27 14:26:11 +0400

    feat: added singleton instance

--- src/TrackerServiceProvider.php
+++ src/TrackerServiceProvider.php
@@ -9,20 +9,20 @@ class TrackerServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-            $this->commands([
-                IssueTestCommand::class
-            ]);
+            $this->commands([IssueTestCommand::class]);
         }
 
         $this->publishes([
-            __DIR__ . '/../config/error-tracker.php' => config_path('error-tracker.php'),
+            __DIR__ . '/../config/error-tracker.php' => config_path('error-tracker.php')
         ]);
+
+        $this->app->singleton('error-tracker', function () {
+            return ErrorTracker::instance();
+        });
     }
 
     public function register()
     {
-        $this->mergeConfigFrom(
-            __DIR__ . '/../config/error-tracker.php', 'error-tracker'
-        );
+        $this->mergeConfigFrom(__DIR__ . '/../config/error-tracker.php', 'error-tracker');
     }
 }
